f82439954cb86d9aa3e0162361daf666d97d7fb6
Code Review Monkey <codereviewmonkey@outlook.com>
2017-12-22 10:45:04 +0100

    Create sketch.js

---
+++ __tests__/sketch.js
@@ -0,0 +1,8 @@
+test('run some code in sketch', (context, document) => {
+  var sketch = context.api()
+
+  log(sketch.api_version)
+  log(sketch.version)
+  log(sketch.build)
+  log(sketch.full_version)
+})
