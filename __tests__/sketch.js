b8abbc44ce901588487c0bc2f5cbabc663ed62e5
Reinier Hartog <rhartog@xebia.com>
2018-06-22 14:39:16 +0200

    Update sketch.js

--- __tests__/sketch.js
+++ __tests__/sketch.js
@@ -1,7 +1,7 @@
 test('run some code in sketch', (context, document) => {
   var sketch = context.api()
 
-  log(sketch.api_version)
+  // log(sketch.api_version)
   log(sketch.version)
   log(sketch.build)
   log(sketch.full_version)
