0cf99312c8be72fe47519f8f9ed2e4fd69fa0fcc
Reinier Hartog <rhartog@xebia.com>
2018-06-22 14:40:15 +0200

    Fixing it

--- __tests__/sketch.js
+++ __tests__/sketch.js
@@ -3,7 +3,7 @@ test('run some code in sketch', (context, document) => {
 
   // log(sketch.api_version)
   log(sketch.version)
-  log(sketch.build)
+  // log(sketch.build)
   log(sketch.full_version)
   
   throw new Error(document._object.fileURL().absoluteString())
